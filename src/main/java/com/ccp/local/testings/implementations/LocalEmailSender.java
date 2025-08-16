207dc6308063c1ffae31d216fe7935ee274c56df
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-08-16 16:25:01 -0300

    declaração de guerra às strings literais soltas nos projetos

--- src/main/java/com/ccp/local/testings/implementations/LocalEmailSender.java
+++ src/main/java/com/ccp/local/testings/implementations/LocalEmailSender.java
@@ -2,12 +2,17 @@ package com.ccp.local.testings.implementations;
 
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.decorators.CcpStringDecorator;
+import com.ccp.decorators.CcpJsonRepresentation.CcpJsonFieldName;
 import com.ccp.especifications.email.CcpEmailSender;
+enum LocalEmailSenderConstants  implements CcpJsonFieldName{
+	templateId
+	
+}
 
 class LocalEmailSender implements CcpEmailSender {
 
 	public CcpJsonRepresentation send(CcpJsonRepresentation emailApiParameters) {
-		String templateId = emailApiParameters.getAsString("templateId");
+		String templateId = emailApiParameters.getAsString(LocalEmailSenderConstants.templateId);
 		new CcpStringDecorator("c:\\logs\\email\\" + templateId + ".json").file().reset().append(emailApiParameters.asPrettyJson());
 		return emailApiParameters;
 	}
