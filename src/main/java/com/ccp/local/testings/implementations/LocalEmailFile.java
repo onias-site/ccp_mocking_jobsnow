a784a3bc11aa94f6c4a7821e7c41f14a52578c24
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-05-22 01:15:43 -0300

    mensagens

--- src/main/java/com/ccp/local/testings/implementations/LocalEmailFile.java
+++ src/main/java/com/ccp/local/testings/implementations/LocalEmailFile.java
@@ -1,19 +1,20 @@
 package com.ccp.local.testings.implementations;
 
+import com.ccp.constantes.CcpOtherConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.decorators.CcpStringDecorator;
 import com.ccp.decorators.CcpJsonRepresentation.CcpJsonFieldName;
 import com.ccp.especifications.email.CcpEmailSender;
+import com.ccp.especifications.http.CcpHttpContentType;
 
 class LocalEmailFile implements CcpEmailSender {
 	enum JsonFieldNames implements CcpJsonFieldName{
 		templateId
 	}
 
-	public CcpJsonRepresentation apply(CcpJsonRepresentation emailApiParameters) {
-		String templateId = emailApiParameters.getAsString(JsonFieldNames.templateId);
-		new CcpStringDecorator("c:\\logs\\email\\" + templateId + ".json").file().reset().append(emailApiParameters.asPrettyJson());
-		return emailApiParameters;
+	public CcpJsonRepresentation sendSimpleTextEmailMessage(String providerToken, String providerUrl, String templateId, String sender, String subject, String message, CcpHttpContentType contentType, String... emails){
+		new CcpStringDecorator("c:\\logs\\email\\" + templateId + ".json").file().reset().append(message);
+		return CcpOtherConstants.EMPTY_JSON;
 	}
 
 }
